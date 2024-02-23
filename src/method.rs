995827cdc12a8ddc53de5ec9e0aa37f1cea213d8
alireza.baloochi1380@gmail.com <alireza.baloochi1380@gmail.com>
2024-02-23 23:26:00 +0330

    add convert from str to Method and convert from Method to str, implement equality between method , str and there refrences

--- src/method.rs
+++ src/method.rs
@@ -1,7 +1,9 @@
 use crate::method::Verb::{*};
 
+#[derive(Debug, PartialEq)]
 pub struct Method(Verb);
 
+#[derive(Debug, PartialEq)]
 enum Verb {
     GET,
     POST,
@@ -11,35 +13,127 @@ enum Verb {
     DELETE,
     HEAD,
     CONNECT,
-    TRACE
+    TRACE,
 }
 
-impl Method{
-
+impl Method {
     ///GET
-    pub const GET : Method = Method(GET);
+    pub const GET: Method = Method(GET);
 
     ///POST
-    pub const POST : Method = Method(POST);
+    pub const POST: Method = Method(POST);
 
     ///PUT
-    pub const PUT : Method = Method(PUT);
+    pub const PUT: Method = Method(PUT);
 
     ///PATCH
-    pub const PATCH : Method = Method(PATCH);
+    pub const PATCH: Method = Method(PATCH);
 
     ///OPTIONS
-    pub const OPTIONS : Method = Method(OPTIONS);
+    pub const OPTIONS: Method = Method(OPTIONS);
 
     ///DELETE
-    pub const DELETE : Method = Method(DELETE);
+    pub const DELETE: Method = Method(DELETE);
 
     ///HEAD
-    pub const HEAD : Method = Method(HEAD);
+    pub const HEAD: Method = Method(HEAD);
 
     ///CONNECT
-    pub const CONNECT : Method = Method(CONNECT);
+    pub const CONNECT: Method = Method(CONNECT);
 
     ///TRACE
-    pub const TRACE : Method = Method(TRACE);
+    pub const TRACE: Method = Method(TRACE);
+
+    /// Return a &str representation of the HTTP method
+    pub fn to_str(&self) -> &str {
+        match self.0 {
+            GET => "GET",
+            POST => "POST",
+            PUT => "PUT",
+            PATCH => "PATCH",
+            OPTIONS => "OPTIONS",
+            DELETE => "DELETE",
+            HEAD => "HEAD",
+            CONNECT => "CONNECT",
+            TRACE => "TRACE"
+        }
+    }
+
+    /// Converts string slice to an HTTP method.
+    pub fn from_str(src: &str) -> Method {
+        match src {
+            "GET" => Method::GET,
+            "POST" => Method::POST,
+            "PUT" => Method::PUT,
+            "PATCH" => Method::PATCH,
+            "OPTIONS" => Method::OPTIONS,
+            "DELETE" => Method::DELETE,
+            "HEAD" => Method::HEAD,
+            "CONNECT" => Method::CONNECT,
+            "TRACE" => Method::TRACE,
+            _ => todo!()
+        }
+    }
+}
+
+
+impl PartialEq<str> for Method {
+    fn eq(&self, other: &str) -> bool {
+        self.to_str() == other
+    }
+}
+
+impl PartialEq<Method> for str {
+    fn eq(&self, other: &Method) -> bool {
+        self == other.to_str()
+    }
+}
+
+
+impl<'a> PartialEq<&'a str> for Method {
+    fn eq(&self, other: &&'a str) -> bool {
+        self.to_str() == *other
+    }
+}
+
+impl<'a> PartialEq<Method> for &'a str {
+    fn eq(&self, other: &Method) -> bool {
+       *self == other.to_str()
+    }
+}
+
+impl<'a> PartialEq<&'a Method> for Method{
+    fn eq(&self, other: &&'a Method) -> bool {
+        self == * other
+    }
+}
+
+impl<'a> PartialEq<Method> for &'a Method{
+    fn eq(&self, other: &Method) -> bool {
+        *self == other
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_method_eq() {
+
+        assert_eq!(Method::GET, Method::GET);
+
+        assert_eq!(Method::GET, Method::from_str("GET"));
+        assert_eq!(Method::GET.to_str(), "GET");
+
+        assert_eq!("GET", Method::GET);
+        assert_eq!(Method::GET,"GET");
+
+        assert_eq!("GET",&Method::GET);
+        assert_eq!(&Method::GET,"GET");
+
+        assert_eq!(Method::GET,&Method::GET);
+        assert_eq!(&Method::GET,Method::GET);
+
+    }
 }
